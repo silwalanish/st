5aec4ecee7ba4468c42e18a466a2c22880c001ba
silwalanish <silwalanish@gmail.com>
2020-03-08 12:24:04 +0545

    Update the font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Menlo for Powerline:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Menlo for Powerline:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
